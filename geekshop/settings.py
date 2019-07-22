4aade86bd629eb4aa77e729ed2a1757c54afb89b
DaniilIssues <severustime@gmail.com>
2019-07-22 15:21:50 +0500

    Кеширование контроллеров и лоу кэш

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -200,3 +200,17 @@ GRAPH_MODELS = {
     'all_applications': True,
     'group_models': True,
 }
+
+if os.name == 'posix':
+   CACHE_MIDDLEWARE_ALIAS = 'default'
+   CACHE_MIDDLEWARE_SECONDS = 120
+   CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
+
+   CACHES = {
+       'default': {
+           'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+           'LOCATION': '127.0.0.1:11211',
+       }
+   }
+
+LOW_CACHE = True
